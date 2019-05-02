893e702178ede536fbc8372ce267e9ba5ea80f70
Maria Korta <mkorta@azti.es>
2019-05-02 15:40:24 +0200

    leaficon script ok

--- dams/draft.R
+++ dams/draft.R
@@ -29,7 +29,7 @@ leaflet(damssf_reproj) %>%
 
 # Pop up list
 # Cedric : Maria is this working ? It doesn't in my computer
-# Maria : as talked, it is because most of the obs_name  == <NA>
+# Maria : as talked, it is because most of the obs_name  == <NA>, it is ok with "id" for example
 leaflet(damssf_reproj) %>% 
   addTiles() %>% 
   addCircleMarkers( #addMarkers
@@ -47,6 +47,9 @@ levels[length(levels) + 1] <- "No available"
 damssf_reproj$obs_name <- factor(damssf_reproj$obs_name, levels = levels)
 damssf_reproj$obs_name[is.na(damssf_reproj$obs_name)] <- "No available"
 
+
+# $obs_height
+damssf_reproj$obs_height <- ifelse(!is.na(damssf_reproj$obs_height),round(damssf_reproj$obs_height, 2),damssf_reproj$obs_height) 
 
 # Maria: MM&CB, which variables do you want to appear in the dams popup?
 
@@ -66,30 +69,31 @@ leaflet(damssf_reproj) %>%
     clusterOptions = markerClusterOptions(), popup = popuplist
   )
 
-
 # specific icons
-
+# Maria: Now shadow and sizes work.
 leafIcons <- icons(
-  iconUrl = ifelse(damssf_reproj$obs_height>10 ,
+  iconUrl = ifelse(damssf_reproj$obs_height>10 & !is.na(damssf_reproj$obs_height),
                    "www/dam.png",
-                   "www/smalldam.png"
-  ),
-  iconWidth = ifelse(damssf_reproj$obs_height>10&!is.na(damssf_reproj$obs_height) , 25,20),
-  iconHeight = ifelse(damssf_reproj$obs_height>10&!is.na(damssf_reproj$obs_height) ,20,10),
-  iconAnchorX = 0, iconAnchorY = 20,
-  shadowUrl =  ifelse(damssf_reproj$obs_height&!is.na(damssf_reproj$obs_height)>10 ,"www/damshadow.png",""),
-   shadowWidth = 40, shadowHeight = 10,
-   shadowAnchorX = 5, shadowAnchorY =0
-)
+                   "www/smalldam.png"),
+      iconWidth = ifelse(damssf_reproj$obs_height>10 & !is.na(damssf_reproj$obs_height) , 25, 20),
+      iconHeight = ifelse(damssf_reproj$obs_height>10 &!is.na(damssf_reproj$obs_height) , 20, 10),
+      iconAnchorX = 0, iconAnchorY = 20,
+  shadowUrl = ifelse(damssf_reproj$obs_height>10 & !is.na(damssf_reproj$obs_height),
+                    "www/damshadow.png",""),
+      shadowWidth = 40,
+      shadowHeight = 10,
+      shadowAnchorX = 5, shadowAnchorY =0
+ )
 
-leaflet(damssf_reproj[1:200,]) %>% 
-  addTiles() %>% 
-  addMarkers(icon = leafIcons)
+# leaflet(damssf_reproj[1:200,]) %>% 
+#   addTiles() %>% 
+#   addMarkers(icon = leafIcons)
+# 
+# 
+# leaflet(damssf_reproj[damssf_reproj$obs_height>10,]) %>% 
+#   addTiles() %>% 
+#   addMarkers(icon = leafIcons)
 
-
-leaflet(damssf_reproj[damssf_reproj$obs_height>10,]) %>% 
-  addTiles() %>% 
-  addMarkers(icon = leafIcons)
 
 # Maria: Popup also works.
 leaflet(damssf_reproj) %>% 
@@ -97,4 +101,8 @@ leaflet(damssf_reproj) %>%
   addMarkers( 
     clusterOptions = markerClusterOptions(),icon = leafIcons, popup = popuplist
   )
+
+
+
+
 
