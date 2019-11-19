94d5d9d6ba5cf7f6fef0446d533d7408b8625827
Maria <mkorta@azti.es>
2019-11-19 14:45:41 +0100

    altitude commit

--- dams/ui.R
+++ dams/ui.R
@@ -23,6 +23,17 @@ library(leafem)
 ## ****************************
 
 load("data/dams.spain.Rdata")
+load("data/altitude.spain.RData")
+
+## ****************************
+## Function for url
+## ****************************
+
+js_code <- "
+shinyjs.browseURL = function(url) {
+  window.open(url,'_blank');
+}
+"
 
 ## ****************************
 ## ui
@@ -53,39 +64,61 @@ load("data/dams.spain.Rdata")
               ),
       
       ##-------
+      ## Login/Signup
+      ##-------
+      
+      tabItem (tabName = "input",
+               fluidRow(column(width = 4, offset = 4,
+                          div(
+                          id="passScreen",
+                           wellPanel(
+                            textInput("user",label = tags$div(HTML('<i class="fa fa-user-circle" style = "color:#000000;"></i> Enter username:'))),
+                            passwordInput("pass", label = tags$div(HTML('<i class="fa fa-lock" style = "color:#000000;"></i> Enter password:'))),
+                             actionButton("okpassword",h4("Login")),
+                             actionButton("okregister",h4("Sign up")),
+                             br()#, hr(),
+                                   #actionLink("passforgot", "Click here if you forgot your password ")
+                             ))))
+                             ),
+  
+      
+      ##-------
       ## Readme
       ##-------
-      tabItem(tabName = "readme",
-              br(),
-              includeHTML ("data/ReadmeSUDOANG.txt")
+      tabItem(tabName = "readme",uiOutput("readmeUi")
+              #br(),
+              #includeHTML ("data/ReadmeSUDOANG.txt")
               ),
       
       ##-----------
       ## Map
       ##-----------
-      tabItem(tabName = "map",
-      fluidRow(
-      column(width = 8,
-           box(width = NULL, status = "primary", solidHeader = TRUE, 
-               leafletOutput("map", height = 800)
-           )),
-      column (width = 4,
-           box(id ="tablebox", width = NULL,status = "primary", title = "Table edits",
-               #tableOutput("table")
-               rHandsontableOutput("table"),
-               br(),
-               actionButton("saveBtn", "Save edits")
-               #downloadButton("downloadExcelSheet", "Send Data"),
-               #verbatimTextOutput("print")
-           ), 
-           box(id ="tablebox2", width = NULL,status = "primary", title = "Table add new ",
-               #tableOutput("table")
-               rHandsontableOutput("table2"),
-               br(),
-               actionButton("saveBtn2", "Save new")
-               )
-    )
-  )
+      tabItem(tabName = "map",uiOutput("mapUi")
+  #     fluidRow(
+  #     column(width = 8,
+  #          box(width = NULL, status = "primary", solidHeader = TRUE, 
+  #              leafletOutput("map", height = 800)
+  #          )),
+  #     column (width = 4,
+  #          box(id ="tablebox", width = NULL,status = "primary", title = "Table edits",
+  #              #tableOutput("table")
+  #              rHandsontableOutput("table"),
+  #              br(),
+  #              actionButton("saveBtn", "Save edits"),
+  #              extendShinyjs(text = js_code, functions = 'browseURL'),
+  #              actionButton ("google", "Google maps")
+  #              #actionButton("google", "Google Maps", onclick ="window.open('http://google.com', '_blank')")
+  #              #downloadButton("downloadExcelSheet", "Send Data"),
+  #              #verbatimTextOutput("print")
+  #          ), 
+  #          box(id ="tablebox2", width = NULL,status = "primary", title = "Table add new ",
+  #              #tableOutput("table")
+  #              rHandsontableOutput("table2"),
+  #              br(),
+  #              actionButton("saveBtn2", "Save new")
+  #              )
+  #   )
+  # )
 )
 )
 )# end of the dashboardbody
@@ -97,6 +130,7 @@ load("data/dams.spain.Rdata")
                 width = 200,
                 id = "tabs",
                 menuItem ("Home", tabName = "home", icon = icon("home"), selected = T),
+                menuItem ("Login/Signup", tabName = "input", icon = icon("sign-in-alt"), selected = F),
                 menuItem ("Readme", tabName = "readme", icon = icon("readme"), selected = F),
                 menuItem ("Map", tabName = "map", icon = icon("map-marker-alt"), selected = F)
               )
@@ -104,38 +138,40 @@ load("data/dams.spain.Rdata")
 
   
  ## All together
-  ui <- fluidPage(
+  ui <- dashboardPage(
     
-  tags$style(type="text/css", "#passScreen {font-size:18px; margin-top: 100px;}"),
+    #fluidPage(
+    
+  #tags$style(type="text/css", "#passScreen {font-size:18px; margin-top: 100px;}"),
   
   useShinyjs(),
   # password panel
   
-  fluidRow(column(width = 4, offset = 4,
-  div(
-    id="passScreen",
-    wellPanel(
-      textInput("user",label = tags$div(HTML('<i class="fa fa-user-circle" style = "color:#000000;"></i> Enter username:'))),
-      passwordInput("pass", label = tags$div(HTML('<i class="fa fa-lock" style = "color:#000000;"></i> Enter password:'))),
-      actionButton("okpassword",h4("Login")),
-      actionButton("okregister",h4("Sign up")),
-      br()#, hr(),
-      #actionLink("passforgot", "Click here if you forgot your password ")
-      )
-    
-  ))),
+  # fluidRow(column(width = 4, offset = 4,
+  # div(
+  #   id="passScreen",
+  #   wellPanel(
+  #     textInput("user",label = tags$div(HTML('<i class="fa fa-user-circle" style = "color:#000000;"></i> Enter username:'))),
+  #     passwordInput("pass", label = tags$div(HTML('<i class="fa fa-lock" style = "color:#000000;"></i> Enter password:'))),
+  #     actionButton("okpassword",h4("Login")),
+  #     actionButton("okregister",h4("Sign up")),
+  #     br()#, hr(),
+  #     #actionLink("passforgot", "Click here if you forgot your password ")
+  #     )
+  #   
+  # ))),
   
   # app showed when password is correct
-  shinyjs::hidden(
-    div(id="myapp",
+  #shinyjs::hidden(
+  #  div(id="myapp",
 
-  dashboardPage(
-    header,
+  #dashboardPage(
+    header = header,
     #dashboardSidebar(disable = TRUE),
-    sidebar,
-    body
-)
-)
-)
+    sidebar = sidebar,
+    body = body
+#)
+#)
+#)
 )# end of the ui
 
